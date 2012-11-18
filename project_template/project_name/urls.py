293d84d0e5436cc880dd5a10814051966c77603f
mmcardle <mark@blueskyops.com>
2012-11-18 22:34:39 +0000

    initial commit

---
+++ project_template/project_name/urls.py
@@ -0,0 +1,21 @@
+from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
+from django.contrib import admin
+from django.conf import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
