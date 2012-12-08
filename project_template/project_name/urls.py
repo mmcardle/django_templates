8a2028e293ecdb5af72cf3b2025e20ae984c2319
mmcardle <mark@blueskyops.com>
2012-12-08 18:21:27 +0000

    Login Page

--- project_template/project_name/urls.py
+++ project_template/project_name/urls.py
@@ -3,13 +3,13 @@ from django.views.generic import TemplateView
 from django.contrib import admin
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.contrib.auth.views import login
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', TemplateView.as_view(template_name="index.html"), name='home'),
-
+    url(r'^login/$', login, {'template_name': 'login.html'}, name='login'),
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
