5c66ed6143513bab9a418500849a1487824afa33
mmcardle <mark.mcardle@sohonet.com>
2013-11-23 09:55:44 +0000

    Add Django-Configrations, bump South

--- project_template_1.6/manage.py
+++ project_template_1.6/manage.py
@@ -3,8 +3,10 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          '{{ project_name }}.settings')
+    os.environ.setdefault('DJANGO_CONFIGURATION', 'Dev')
 
-    from django.core.management import execute_from_command_line
+    from configurations.management import execute_from_command_line
 
     execute_from_command_line(sys.argv)
--- project_template_1.6/project_name/settings.py
+++ project_template_1.6/project_name/settings.py
@@ -10,185 +10,236 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
+from configurations import Configuration
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
-)
+class Base(Configuration):
 
-MANAGERS = ADMINS
+    PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 
-DEBUG = True
-TEMPLATE_DEBUG = True
+    DEBUG = True
 
-SITE_ID = 1
+    # Quick-start development settings - unsuitable for production
+    # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-ALLOWED_HOSTS = []
+    ADMINS = (
+        # ('Your Name', 'your_email@example.com'),
+    )
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+    MANAGERS = ADMINS
 
-# URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# trailing slash.
-# Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = '/media/'
+    TEMPLATE_DEBUG = True
 
-# Absolute path to the directory static files should be collected to.
-# Don't put anything in this directory yourself; store your static files
-# in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+    SITE_ID = 1
 
-# URL prefix for static files.
-# Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+    ALLOWED_HOSTS = []
 
-# Additional locations of static files
-STATICFILES_DIRS = (
-    # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, 'static'),
-)
+    # Absolute filesystem path to the directory that will
+    # hold user-uploaded files.
+    # Example: "/var/www/example.com/media/"
+    MEDIA_ROOT = ''
 
-# List of finder classes that know how to find static files in
-# various locations.
-STATICFILES_FINDERS = (
-    'django.contrib.staticfiles.finders.FileSystemFinder',
-    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-    'compressor.finders.CompressorFinder',
-)
+    # URL that handles the media served from MEDIA_ROOT. Make sure to use a
+    # trailing slash.
+    # Examples: "http://example.com/media/", "http://media.example.com/"
+    MEDIA_URL = '/media/'
 
-# Make this unique, and don't share it with anybody.
-SECRET_KEY = '2(n=3ojff0m(5a)g(g5uslf4e+b=vxjulz%iid+%=s%&uuhcys'
+    # Absolute path to the directory static files should be collected to.
+    # Don't put anything in this directory yourself; store your static files
+    # in apps' "static/" subdirectories and in STATICFILES_DIRS.
+    # Example: "/var/www/example.com/static/"
+    STATIC_ROOT = ''
 
-# List of callables that know how to import templates from various sources.
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-)
+    # URL prefix for static files.
+    # Example: "http://example.com/static/", "http://static.example.com/"
+    STATIC_URL = '/static/'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates"
-    # or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    os.path.join(PROJECT_DIR, 'templates'),
-)
+    # Additional locations of static files
+    STATICFILES_DIRS = (
+        # Put strings here, like "/home/html/static" or "C:/www/django/static".
+        # Always use forward slashes, even on Windows.
+        # Don't forget to use absolute paths, not relative paths.
+        os.path.join(PROJECT_DIR, 'static'),
+    )
 
-# Application definition
+    # List of finder classes that know how to find static files in
+    # various locations.
+    STATICFILES_FINDERS = (
+        'django.contrib.staticfiles.finders.FileSystemFinder',
+        'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+        'compressor.finders.CompressorFinder',
+    )
 
-INSTALLED_APPS = (
-    'admin_tools',
-    'admin_tools.theming',
-    'admin_tools.menu',
-    'admin_tools.dashboard',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.sites',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    # Uncomment the next line to enable the admin:
-    'django.contrib.admin',
-    # Uncomment the next line to enable admin documentation:
-    'django.contrib.admindocs',
+    # Make this unique, and don't share it with anybody.
+    SECRET_KEY = '2(n=3ojff0m(5a)g(g5uslf4e+b=vxjulz%iid+%=s%&uuhcys'
 
-    # Apps
-    'django_extensions',
-    'south',
-    'crispy_forms',
-    'django_static',
-    'compressor'
-)
+    # List of callables that know how to import templates from various sources.
+    TEMPLATE_LOADERS = (
+        'django.template.loaders.filesystem.Loader',
+        'django.template.loaders.app_directories.Loader',
+    )
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+    TEMPLATE_DIRS = (
+        # Put strings here, like "/home/html/django_templates"
+        # or "C:/www/django/templates".
+        # Always use forward slashes, even on Windows.
+        # Don't forget to use absolute paths, not relative paths.
+        os.path.join(PROJECT_DIR, 'templates'),
+    )
 
-TEMPLATE_CONTEXT_PROCESSORS = (
-    # default template context processors
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    # required by django-admin-tools
-    'django.core.context_processors.request',
-)
+    # Application definition
 
-ROOT_URLCONF = '{{ project_name }}.urls'
+    INSTALLED_APPS = (
+        'admin_tools',
+        'admin_tools.theming',
+        'admin_tools.menu',
+        'admin_tools.dashboard',
+        'django.contrib.auth',
+        'django.contrib.contenttypes',
+        'django.contrib.sessions',
+        'django.contrib.sites',
+        'django.contrib.messages',
+        'django.contrib.staticfiles',
+        # Uncomment the next line to enable the admin:
+        'django.contrib.admin',
+        # Uncomment the next line to enable admin documentation:
+        'django.contrib.admindocs',
 
-WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+        # Apps
+        'django_extensions',
+        'south',
+        'crispy_forms',
+        'django_static',
+        'compressor'
+    )
 
-# Configure App - django_static
-DJANGO_STATIC = True
-DJANGO_STATIC_MEDIA_ROOTS = [PROJECT_DIR]
+    MIDDLEWARE_CLASSES = (
+        'django.contrib.sessions.middleware.SessionMiddleware',
+        'django.middleware.common.CommonMiddleware',
+        'django.middleware.csrf.CsrfViewMiddleware',
+        'django.contrib.auth.middleware.AuthenticationMiddleware',
+        'django.contrib.messages.middleware.MessageMiddleware',
+        'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    )
 
-# Database
-# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+    TEMPLATE_CONTEXT_PROCESSORS = (
+        # default template context processors
+        'django.contrib.auth.context_processors.auth',
+        'django.core.context_processors.debug',
+        'django.core.context_processors.i18n',
+        'django.core.context_processors.media',
+        'django.core.context_processors.static',
+        # required by django-admin-tools
+        'django.core.context_processors.request',
+    )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(PROJECT_DIR, '{{ project_name }}.sqlite3'),
+    ROOT_URLCONF = '{{ project_name }}.urls'
+
+    WSGI_APPLICATION = '{{ project_name }}.wsgi.application'
+
+    # Configure App - django_static
+    DJANGO_STATIC = True
+    DJANGO_STATIC_MEDIA_ROOTS = [PROJECT_DIR]
+
+    # Database
+    # https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(PROJECT_DIR, '{{ project_name }}.sqlite3'),
+        }
     }
-}
 
-# Internationalization
-# https://docs.djangoproject.com/en/1.6/topics/i18n/
+    # Internationalization
+    # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+    LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+    TIME_ZONE = 'UTC'
 
-USE_I18N = True
+    USE_I18N = True
 
-USE_L10N = True
+    USE_L10N = True
 
-USE_TZ = True
+    USE_TZ = True
 
-# A sample logging configuration. The only tangible logging
-# performed by this configuration is to send an email to
-# the site admins on every HTTP 500 error when DEBUG=False.
-# See http://docs.djangoproject.com/en/dev/topics/logging for
-# more details on how to customize your logging configuration.
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'filters': {
-        'require_debug_false': {
-            '()': 'django.utils.log.RequireDebugFalse'
-        }
-    },
-    'handlers': {
-        'mail_admins': {
-            'level': 'ERROR',
-            'filters': ['require_debug_false'],
-            'class': 'django.utils.log.AdminEmailHandler'
-        }
-    },
-    'loggers': {
-        'django.request': {
-            'handlers': ['mail_admins'],
-            'level': 'ERROR',
-            'propagate': True,
+    # A sample logging configuration. The only tangible logging
+    # performed by this configuration is to send an email to
+    # the site admins on every HTTP 500 error when DEBUG=False.
+    # See http://docs.djangoproject.com/en/dev/topics/logging for
+    # more details on how to customize your logging configuration.
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'filters': {
+            'require_debug_false': {
+                '()': 'django.utils.log.RequireDebugFalse'
+            }
         },
+        'handlers': {
+            'mail_admins': {
+                'level': 'ERROR',
+                'filters': ['require_debug_false'],
+                'class': 'django.utils.log.AdminEmailHandler'
+            }
+        },
+        'loggers': {
+            'django.request': {
+                'handlers': ['mail_admins'],
+                'level': 'ERROR',
+                'propagate': True,
+            },
+        }
     }
-}
 
-try:
-    from local_settings import *
-    INSTALLED_APPS += DEBUG_APPS
-    MIDDLEWARE_CLASSES += DEBUG_MIDDLEWARE_CLASSES
-except:
-    pass
+
+class Dev(Base):
+
+    DEBUG = True
+    DJANGO_STATIC = not DEBUG
+
+    INSTALLED_APPS = Base.INSTALLED_APPS + (
+        "debug_toolbar",
+    )
+
+    MIDDLEWARE_CLASSES = Base.MIDDLEWARE_CLASSES + (
+        'debug_toolbar.middleware.DebugToolbarMiddleware',
+    )
+
+    DEBUG_TOOLBAR_PANELS = (
+        'debug_toolbar.panels.version.VersionDebugPanel',
+        'debug_toolbar.panels.timer.TimerDebugPanel',
+        'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+        'debug_toolbar.panels.headers.HeaderDebugPanel',
+        'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+        'debug_toolbar.panels.template.TemplateDebugPanel',
+        'debug_toolbar.panels.sql.SQLDebugPanel',
+        'debug_toolbar.panels.signals.SignalDebugPanel',
+        'debug_toolbar.panels.logger.LoggingPanel',
+    )
+
+
+    def custom_show_toolbar(request):
+        return Base.DEBUG  # Always show toolbar, for example purposes only.
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'HIDE_DJANGO_SQL': False,
+        'ENABLE_STACKTRACES' : True,
+        'SHOW_TOOLBAR_CALLBACK': custom_show_toolbar
+    }
+
+    DATABASES = {
+        'default': {
+            # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+            'ENGINE': 'django.db.backends.sqlite3',
+            # Or path to database file if using sqlite3.
+            'NAME': '{{ project_name }}.sqlite3',
+        }
+    }
+
+
+class Prod(Base):
+    DEBUG = False
+    ALLOWED_HOSTS = []
